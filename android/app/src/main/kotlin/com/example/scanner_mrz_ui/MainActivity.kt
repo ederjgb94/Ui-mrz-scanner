dfe3dccad29f1e6db9cbcf5a2e9ba1aece84c69f
ederjgb94 <a2133330155@alumnos.uat.edu.mx>
2022-03-02 08:54:25 -0600

    firstinit

---
+++ android/app/src/main/kotlin/com/example/scanner_mrz_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.scanner_mrz_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/styles.dart
@@ -0,0 +1,8 @@
+class MyColors {
+  static const int goldColor = 0xffd3a01f;
+  static const int background = 0xff121212;
+  static const int greyground = 0xff202020;
+  static const int textColor = 0xffe0e0e0;
+
+  MyColors._();
+}
